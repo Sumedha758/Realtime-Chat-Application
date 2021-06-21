fa6165b64f7a807a24ca444775aed8f1404f255d
Sumedha Mishra <sumedhamishra64@gmail.com>
2021-06-21 13:24:03 +0530

    chat.js modified

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -29,8 +29,10 @@ const Chat =({ location }) =>{
         setName(name);
         setRoom(room);
 
-        socket.emit('join', {name, room}, () => {
-    
+        socket.emit('join', {name, room}, (error) => {
+            if(error) {
+                alert(error);
+              }
         });
 
         return () => {
